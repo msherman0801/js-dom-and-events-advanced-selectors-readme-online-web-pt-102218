d30d4f35a5535d95e00a04dabdfdbad7bb009bea
Matthew Sherman <msherman0801@gmail.com>
2019-05-29 13:35:43 -0400

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+
+function paragraphSelector() {
+    return $("p");
+}
+
+function lastImageSelector() {
+    return $(".pics img:last");
+}
+
+function ninjaBabySelector() {
+    return $("#baby-ninja")
+}
+
+function divSelector() {
+    let arr = []
+    let index = 0
+    const out = $("div")
+    while (index < 2) {
+        arr.push(out[index])
+        index++
+    }
+    return arr
+}
+
+function firstListItem() {
+    return $("li:first-child")
+}
